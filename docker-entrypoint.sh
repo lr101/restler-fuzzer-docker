e99ce7767334d2113e606cbf0cdb5a2435effa8a
Lukas Reim <lukasr101@gmail.com>
2023-09-28 15:31:54 +0200

    change entrypoint definition

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /RESTler/restler/Restler compile --api_spec /mnt/shared/${OPEN_API_FILE}
-/RESTler/restler/Restler test --grammar_file ./Compile/grammar.py --dictionary_file ./Compile/dict.json --settings ./Compile/engine_settings.json --target_ip ${SERVER:="host.docker.internal"} --target_port ${PORT} --no_ssl
-/RESTler/restler/Restler fuzz --grammar_file ./Compile/grammar.py --dictionary_file ./Compile/dict.json --settings ./Compile/engine_settings.json --time_budget ${TIME_BUDGET} --target_ip ${SERVER:="host.docker.internal"} --target_port ${PORT} --no_ssl
+/RESTler/restler/Restler test --grammar_file ./Compile/grammar.py --dictionary_file ./Compile/dict.json --settings /mnt/shared/settings.json --target_ip ${SERVER:="host.docker.internal"} --target_port ${PORT} --no_ssl
+/RESTler/restler/Restler fuzz --grammar_file ./Compile/grammar.py --dictionary_file ./Compile/dict.json --settings /mnt/shared/settings.json --time_budget ${TIME_BUDGET} --target_ip ${SERVER:="host.docker.internal"} --target_port ${PORT} --no_ssl
